dd6f7bba0cfba564d26f28d68af97217d0b7b17d
Veeti Paananen <veeti.paananen@rojekti.fi>
2014-06-27 08:24:57 +0300

    Clean up imports

--- jyramid-framework/src/main/java/com/jyramid/config/Application.java
+++ jyramid-framework/src/main/java/com/jyramid/config/Application.java
@@ -4,7 +4,6 @@ import com.jyramid.http.HttpRequestImpl;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Application {
